2b9bfad7e1fe7c7d1df4f41002862bbf16967298
wukewei <565389849@qq.com>
2016-12-13 22:33:48 +0800

    change androidlist

--- src/components/AndroidList.js
+++ src/components/AndroidList.js
@@ -4,53 +4,20 @@
  */
 import React,{Component} from 'react';
 import { ListView } from 'antd-mobile';
-
-const data = [
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-    title: '相约酒店',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-    title: '相约酒店',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-    title: '相约酒店',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-    title: '相约酒店',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-    title: '相约酒店',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
-    title: '麦当劳邀您过周末',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-  {
-    img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
-    title: '食惠周',
-    des: '不是所有的兼职汪都需要风吹日晒',
-  },
-];
+import Spinner from "./Spinner";
+import type {Android} from '../flowtype/index';
+import {PAGE_SIZE} from '../constants/Constants';
 
 type Props = {
-  list : Array<MeiZhi>,
+  list : Array<Android>,
   isFetching: boolean,
   fetchDataCallback : (pageSize: number) => void
 };
 
-class Android extends Component {
+let pageSize: number = 1;
+let loadMoreTime: number = 0;
+
+class AndroidList extends Component {
 
   state: {
     dataSource: ListView.DataSource
@@ -58,7 +25,6 @@ class Android extends Component {
 
   constructor(props: Props) {
     super(props);
-
     this.state =  {
       dataSource:  new ListView.DataSource({
         rowHasChanged: (row1, row2) => row1 !== row2,
@@ -68,65 +34,84 @@ class Android extends Component {
 
 
   componentDidMount() {
-    this.setState = {
-      dataSource: this.state.dataSource.cloneWithRows(data)
-    }
+    this.props.fetchDataCallback(pageSize)
   }
 
 
-  onEndReached(event) {
+  onEndReached() {
     // load new data
-    console.log('reach end', event);
+    const time: number  = (+new Date() ) / 1000;
+    if (time - loadMoreTime > 1) {
+      pageSize ++;
+      loadMoreTime = (+new Date() ) / 1000;
+      this.props.fetchDataCallback(pageSize);
+    }
+
+  }
+
+  renderSpinner() {
+    const list = this.props.list;
+    const isFetching = this.props.isFetching;
+
+    if (list && list.length > 0 && isFetching) {
+      return <Spinner />
+    }
+    return null;
+  }
+
+  renderContent(rowData: Android) {
+    if (rowData.images && rowData.images.length > 0) {
+
+      return (
+        <div>
+        <a className="meizhi-list-item">
+          <img
+            className="meizhi-list-item__image"
+            src={rowData.images[0]}
+          />
+        </a>
+          <div>
+            {rowData.images[0]}
+          </div>
+        </div>
+      );
+
+    }
+
+
+    return (
+        <div>
+          {rowData.publishedAt}
+        </div>
+    );
 
   }
 
   render() {
-
-    const row = (rowData, sectionID, rowID) => {
+    const list = this.props.list;
+    if (list && list.length > 0) {
       return (
-        <div key={rowID}
-             style={{
-            padding: '8px 16px',
-            backgroundColor: 'white',
-          }}
-        >
-          <h3 style={{ padding: 2, marginBottom: 8, borderBottom: '1px solid #F6F6F6' }}>
-            {rowID}
-          </h3>
-          <div style={{ display: '-webkit-box', display: 'flex' }}>
-            <div style={{ display: 'inline-block' }}>
-              <p>wukeweiwekdnak</p>
-              <p><span style={{ fontSize: '1.6em', color: '#FF6E27' }}>35</span>元/任务</p>
-            </div>
-          </div>
-        </div>
-      );
-    };
-
-
-
-    return (<div style={{ margin: '0 auto', width: '96%' }}>
-      <ListView
-        dataSource={this.state.dataSource.cloneWithRows(data)}
-        renderFooter={() => <div style={{ padding: 30, textAlign: 'center' }}>
-          {this.state.isLoading ? '加载中...' : '加载完毕'}
-        </div>}
-        renderRow={row}
-        className="fortest"
-        style={{
-          height: document.body.clientHeight,
+        <ListView
+          dataSource={this.state.dataSource.cloneWithRows(list)}
+          renderFooter={() => this.renderSpinner()}
+          renderRow={this.renderContent}
+          style={{
+          height: document.body.clientHeight - 50,
           overflow: 'auto',
           border: '1px solid #ddd',
           margin: '10px 0',
         }}
-        pageSize={10}
-        scrollRenderAheadDistance={500}
-        scrollEventThrottle={20}
-        onScroll={() => { console.log('scroll'); }}
-        onEndReachedThreshold={10}
-      />
-    </div>);
+          pageSize={PAGE_SIZE}
+          scrollRenderAheadDistance={500}
+          scrollEventThrottle={20}
+          onEndReached={() => this.onEndReached()}
+          onEndReachedThreshold={10}
+        />
+      )
+    } else {
+      return (<Spinner />);
+    }
   }
 }
 
-export default Android;
+export default AndroidList;
